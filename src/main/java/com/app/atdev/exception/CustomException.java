deff2343d71579f8efe1997d67e15873de6e1ed9
Mandeep Sharma <142305917+mandeeep19@users.noreply.github.com>
2024-03-28 01:34:08 +0530

    Update CustomException.java

--- src/main/java/com/app/atdev/exception/CustomException.java
+++ src/main/java/com/app/atdev/exception/CustomException.java
@@ -2,7 +2,7 @@ package com.app.atdev.exception;
 
 public class CustomException extends RuntimeException{
 	
-	public CustomException(String message)
+	public CustomException(String message) // It handles the custom exception
 	{
 		super(message);
 	}
